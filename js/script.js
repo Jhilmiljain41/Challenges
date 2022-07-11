8ba72987c876d54cf76c231bc74284d18fbe1a49
Jhilmiljain41 <jhilmiljain41@gmail.com>
2022-07-11 20:10:17 +0530

    added changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,45 @@
 
 $(document).ready(function() {
-    $(".nav li a").on('click', function(e) {
+    $(".active-nav li a").on('click', function(e) {
         e.preventDefault()
         var page = $(this).data('page');
         $("#pages .page:not('hide')").stop().fadeOut('fast', function() {
             $(this).addClass('hide');
             $('#pages .page[data-page="'+page+'"]').fadeIn('slow').removeClass('hide');
         });
-        $(this).removeClass('hide');
+    });
+});
+
+$(document).ready(function() {
+    $(".side-bar span a").on('click', function(e) {
+        e.preventDefault()
+        var pages = $(this).data('page');
+        $("#page-change .pagebar:not('hides')").stop().fadeOut('fast', function() {
+            $(this).addClass('hides');
+            $('#page-change .pagebar[data-page="'+pages+'"]').fadeIn('slow').removeClass('hide');
+        });
+    });
+});
+
+$(document).ready(function() {
+    $(".top-bar li a").on('click', function(e) {
+        e.preventDefault()
+        var page = $(this).data('page');
+        $("#page01 .pages:not('hide')").stop().fadeOut('fast', function() {
+            $(this).addClass('hide');
+            $('#page01 .pages[data-page="'+page+'"]').fadeIn('slow').removeClass('hide');
+        });
+    });
+});
+
+$(document).ready(function() {
+    $(".top-bar02 li a").on('click', function(e) {
+        e.preventDefault()
+        var page = $(this).data('page');
+        $("#page02 .board:not('hide')").stop().fadeOut('fast', function() {
+            $(this).addClass('hide');
+            $('#page02 .board[data-page="'+page+'"]').fadeIn('slow').removeClass('hide');
+        });
     });
 });
 
